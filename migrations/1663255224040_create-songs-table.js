a3aae0a77233085fb5a3be68508c7d0d41b3f9cb
TiveCS <ahmadfathanhanif@gmail.com>
2022-09-15 22:33:20 +0700

    create database schema

---
+++ migrations/1663255224040_create-songs-table.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    performer: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    genre: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+      notNull: false,
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      notNull: false,
+      references: '"albums"',
+      onDelete: 'cascade',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
